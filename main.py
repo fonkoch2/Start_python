5adf8fd00969ae2b57d516e8a192448ab5b713a4
fonkoch2 <fonkoch0@mail.ru>
2019-08-17 11:14:26 +0300

    исправка ошибки

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-def  calc(x,y):
-   return x+y
+def calc(x, y):
+    return x+y
 
-def render(x,y):
-   print(x,y)
 
-def pow(x,y):
-      return x**y
+def render(x, y):
+    print(x, y)
 
-def wrap_clac(func):
-   def result_func(x,y):
-      x=x**2
-      y=y**2
 
-      return func(x,y) 
+def pow(x, y):
+    return x**y
 
-return resuslt_func    
-   
+
+def wrap_calc(func):
+   def result_func(x, y):
+         x = x**2
+         y = y**2
+
+         return func(x, y)
+
+   return result_func
+
+
 if __name__ == '__main__':
-   ready_func = wrap_calc(calc)
-   print(calc(4,5))
-   print(ready_func(4,5))
+    ready_func = wrap_calc(render)
 
-      
+    print(render(4, 5))
+    print(ready_func(4, 5))
