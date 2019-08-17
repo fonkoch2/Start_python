7828235e714e83ae86562280c53b0ec52e56fbee
fonkoch2 <fonkoch0@mail.ru>
2019-08-17 11:05:50 +0300

    add  decorator

---
+++ main.py
@@ -0,0 +1,24 @@
+def  calc(x,y):
+   return x+y
+
+def render(x,y):
+   print(x,y)
+
+def pow(x,y):
+      return x**y
+
+def wrap_clac(func):
+   def result_func(x,y):
+      x=x**2
+      y=y**2
+
+      return func(x,y) 
+
+return resuslt_func    
+   
+if __name__ == '__main__':
+   ready_func = wrap_calc(calc)
+   print(calc(4,5))
+   print(ready_func(4,5))
+
+      
